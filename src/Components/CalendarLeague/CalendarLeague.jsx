c3a5287ff082e6e4aa08d928950b92793531cce6
Runar <runarnazmutdinov@gmail.com>
2022-01-30 11:27:19 +0300

    feat: slight changes

--- src/Components/CalendarLeague/CalendarLeague.jsx
+++ src/Components/CalendarLeague/CalendarLeague.jsx
@@ -1,45 +1,44 @@
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 import { MaterialTable } from "../../MaterialUI/MaterialTable";
 import { MaterialDateFilter } from "../../MaterialUI/MaterialDateFilter";
 import { useRequest } from "../../useRequest";
+import Dayjs from "dayjs";
 import Spin from "../../Spin";
 
 export const CalendarLeague = () => {
   const params = useParams();
+  let location = useLocation();
+  let navigate = useNavigate();
   const teamId = params.id;
   const { data, loading, errorMessage } = useRequest(
     `https://api.football-data.org/v2/competitions/${teamId}/matches`
   );
 
-  const dateFrom = new URLSearchParams(window.location.search).get("dateFrom");
-  const dateTo = new URLSearchParams(window.location.search).get("dateTo");
+  const dateFrom = new URLSearchParams(location.search).get("dateFrom");
+  const dateTo = new URLSearchParams(location.search).get("dateTo");
 
-  const [selectDateFrom, setDateFrom] = useState(parseInt(dateFrom) || null);
-  const [selectDateTo, setDateTo] = useState(parseInt(dateTo) || null);
+  const [selectDateFrom, setDateFrom] = useState(
+    +Dayjs(dateFrom).valueOf() || null
+  );
+  const [selectDateTo, setDateTo] = useState(+Dayjs(dateTo).valueOf() || null);
 
   const handleChangeDateFrom = (date) => {
-    window.history.pushState(
-      "Object",
-      "Title",
-      `?dateFrom=${date && date.getTime()}&dateTo=${dateTo}`
-    );
+    navigate(`?dateFrom=${Dayjs(date).format("MM-DD-YYYY")}&dateTo=${dateTo}`);
     setDateFrom(date);
   };
 
   const handleChangeDateTo = (date) => {
-    window.history.pushState(
-      "Object",
-      "Title",
-      `?dateTo=${date && date.getTime()}&dateFrom=${dateFrom}`
+    navigate(
+      `?dateTo=${Dayjs(date).format("MM-DD-YYYY")}&dateFrom=${dateFrom}`
     );
     setDateTo(date);
   };
 
   const matchDate = data?.matches?.filter((match) => {
     return (
-      Date.parse(match.utcDate) >= dateFrom &&
-      Date.parse(match.utcDate) <= dateTo
+      +Dayjs(match.utcDate).valueOf() >= selectDateFrom &&
+      +Dayjs(match.utcDate).valueOf() <= selectDateTo
     );
   });
 
--- src/Components/TeamCalendar/TeamCalendar.jsx
+++ src/Components/TeamCalendar/TeamCalendar.jsx
@@ -1,46 +1,44 @@
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 import { MaterialTable } from "../../MaterialUI/MaterialTable";
 import { MaterialDateFilter } from "../../MaterialUI/MaterialDateFilter";
+import Dayjs from "dayjs";
 import Spin from "../../Spin";
 import { useRequest } from "../../useRequest";
 
 export const TeamCalendar = () => {
   const params = useParams();
   const teamId = params.id;
-
+  const location = useLocation();
+  const navigate = useNavigate();
   const { data, loading, errorMessage } = useRequest(
     `https://api.football-data.org/v2/teams/${teamId}/matches`
   );
 
-  const dateFrom = new URLSearchParams(window.location.search).get("dateFrom");
-  const dateTo = new URLSearchParams(window.location.search).get("dateTo");
+  const dateFrom = new URLSearchParams(location.search).get("dateFrom");
+  const dateTo = new URLSearchParams(location.search).get("dateTo");
 
-  const [selectDateFrom, setDateFrom] = useState(Number(dateFrom) || null);
-  const [selectDateTo, setDateTo] = useState(Number(dateTo) || null);
+  const [selectDateFrom, setDateFrom] = useState(
+    +Dayjs(dateFrom).valueOf() || null
+  );
+  const [selectDateTo, setDateTo] = useState(+Dayjs(dateTo).valueOf() || null);
 
   const handleChangeDateFrom = (date) => {
-    window.history.pushState(
-      "Object",
-      "Title",
-      `?dateFrom=${date && date.getTime()}&dateTo=${dateTo}`
-    );
+    navigate(`?dateFrom=${Dayjs(date).format("MM-DD-YYYY")}&dateTo=${dateTo}`);
     setDateFrom(date);
   };
 
   const handleChangeDateTo = (date) => {
-    window.history.pushState(
-      "Object",
-      "Title",
-      `?dateTo=${date && date.getTime()}&dateFrom=${dateFrom}`
+    navigate(
+      `?dateTo=${Dayjs(date).format("MM-DD-YYYY")}&dateFrom=${dateFrom}`
     );
     setDateTo(date);
   };
 
   const matchDate = data?.matches?.filter((match) => {
     return (
-      Date.parse(match.utcDate) >= dateFrom &&
-      Date.parse(match.utcDate) <= dateTo
+      +Dayjs(match.utcDate).valueOf() >= selectDateFrom &&
+      +Dayjs(match.utcDate).valueOf() <= selectDateTo
     );
   });
 
